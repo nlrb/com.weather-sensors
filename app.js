319c6ef9a5076b2760f78b0e81ba9982e3261f7c
nlrb <github@ramonbaas.nl>
2016-10-24 12:15:39 +0200

    Add support for Alecto v3

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const oregon = require('oregon');
+const alecto = require('alecto');
 const cresta = require('cresta');
+const oregon = require('oregon');
 const utils = require('utils');
 const sensor = require('./drivers/sensor.js');
 
 const protocols = { 
 	oregonv2: { name: 'Oregon Scientific v2', signal: 'OregonV2', parser: oregon.parsev2 },
 	oregonv3: { name: 'Oregon Scientific v3', signal: 'OregonV3', parser: oregon.parsev3 },
-	cresta: { name: 'Cresta / TFA', signal: 'Cresta', parser: cresta.parse }
+	cresta: { name: 'Cresta / TFA', signal: 'Cresta', parser: cresta.parse },
+	alecto: { name: 'Alecto v3', signal: 'AlectoV3', parser: alecto.parsev3 }
 };
 const locale = Homey.manager('i18n').getLanguage() == 'nl' ? 'nl' : 'en'; // only Dutch & English supported
 
